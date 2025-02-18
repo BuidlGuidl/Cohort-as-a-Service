add98139ac9ebb60808d25b574aa9af8a35078f0
avelous <avelousujiri@gmail.com>
2025-02-18 16:58:01 +0100

    cleanup

--- packages/nextjs/app/(routes)/cohort/[cohortAddress]/members/page.tsx
+++ packages/nextjs/app/(routes)/cohort/[cohortAddress]/members/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { StreamContractInfo } from "../_components/StreamContractInfo";
 import { CreatorsList } from "./_components/CreatorsList";
 import { EventsModal } from "./_components/EventsModal";
