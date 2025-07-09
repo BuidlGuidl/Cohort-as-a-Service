e041a7ec0ee1abb368de4e26d404a453c4158101
Zak G <zakgriffith@gmail.com>
2025-07-09 12:03:18 -0600

    View Projects->Add Projects when no projects, center and enlarge connet wallet button, center chain names and use grid on small resolutions

--- packages/nextjs/app/(routes)/cohorts/_components/Chains.tsx
+++ packages/nextjs/app/(routes)/cohorts/_components/Chains.tsx
@@ -8,20 +8,24 @@ interface ChainListProps {
 
 const ChainsLoader = () => {
   return (
-    <div className="flex items-center gap-x-2 overflow-x-auto pb-2 ">
-      {[1, 2].map(i => (
-        <div key={i} className="h-8 w-24 rounded-lg bg-gray-200 animate-pulse" />
-      ))}
+    <div className="flex justify-center">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:flex lg:items-center lg:gap-x-2 lg:overflow-x-auto pb-2">
+        {[1, 2, 3, 4, 5, 6].map(i => (
+          <div key={i} className="h-8 w-full rounded-lg bg-gray-200 animate-pulse" />
+        ))}
+      </div>
     </div>
   );
 };
 
 const ChainsList = ({ items }: ChainListProps) => {
   return (
-    <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
-      {items.map(item => (
-        <ChainItem key={item.chainId} chainId={item.chainId} icon={item.icon} name={item.name} />
-      ))}
+    <div className="flex justify-center">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:flex lg:items-center lg:gap-x-2 lg:overflow-x-auto pb-2">
+        {items.map(item => (
+          <ChainItem key={item.chainId} chainId={item.chainId} icon={item.icon} name={item.name} />
+        ))}
+      </div>
     </div>
   );
 };
