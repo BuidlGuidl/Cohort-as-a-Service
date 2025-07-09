6fc50ef2c771fb8e17fde9d9717eaf09d118f3fe
avelous <avelousujiri@gmail.com>
2025-07-09 09:16:52 +0100

    /create changed to /deploy

--- packages/nextjs/app/(routes)/deploy/page.tsx
+++ packages/nextjs/app/(routes)/deploy/page.tsx
@@ -1,15 +1,9 @@
-import Link from "next/link";
 import ChainToggler from "./_components/ChainToggler";
 import CreateCohortForm from "./_components/CreateCohortForm";
-import { ArrowLongLeftIcon } from "@heroicons/react/24/outline";
 
 const CreatePage = () => {
   return (
     <div className="max-w-4xl mt-10 space-y-6 mx-auto">
-      <Link href="/app" className="btn btn-ghost btn-sm rounded-sm">
-        <ArrowLongLeftIcon className="w-7 h-4" />
-        Home
-      </Link>
       <h1 className="text-2xl font-semibold">Create a new cohort</h1>
       <ChainToggler />
       <CreateCohortForm />
@@ -18,3 +12,4 @@ const CreatePage = () => {
 };
 
 export default CreatePage;
+
