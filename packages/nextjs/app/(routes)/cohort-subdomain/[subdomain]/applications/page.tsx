9227f757ff4e7384a3fed1586d8bf6c96c1afd59
avelous <avelousujiri@gmail.com>
2025-08-21 10:30:47 +0100

    fix subdomain uniqueness issue

--- packages/nextjs/app/(routes)/cohort-subdomain/[subdomain]/applications/page.tsx
+++ packages/nextjs/app/(routes)/cohort-subdomain/[subdomain]/applications/page.tsx
@@ -3,7 +3,7 @@ import ApplicationsPage from "../../../cohort/[cohortAddress]/applications/page"
 import db from "~~/lib/db";
 
 export default async function SubdomainApplicationsPage({ params }: { params: { subdomain: string } }) {
-  const cohort = await db.cohort.findUnique({
+  const cohort = await db.cohort.findFirst({
     where: { subdomain: params.subdomain.toLowerCase() },
   });
 
